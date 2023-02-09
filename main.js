703601ac2f32cfe7db36713373f700ffcf7ef172
Loli <lolarlew@gmail.com>
2023-02-09 14:44:59 -0300

    working on input message

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 const portfolio = document.getElementById("portfolio");
 
+/*
 portfolio.addEventListener("click", () => {
   Swal.fire({
     input: 'textarea',
@@ -8,11 +9,31 @@ portfolio.addEventListener("click", () => {
     inputPlaceholder: 'Type your message here...',
     inputAttributes: {
       'aria-label': 'Type your message here'
-    },
-    showCancelButton: true
+    }, 
+    showCancelButton: true 
   })
   
   if (text) {
     Swal.fire(text)
   }  
 })
+*/
+
+portfolio.addEventListener("click", () => 
+const { value: formValues } = await Swal.fire({
+  title: 'Multiple inputs',
+  html:
+    '<input id="swal-input1" class="swal2-input">' +
+    '<input id="swal-input2" class="swal2-input">',
+  focusConfirm: false,
+  preConfirm: () => {
+    return [
+      document.getElementById('swal-input1').value,
+      document.getElementById('swal-input2').value
+    ]
+  }
+})
+if (formValues) {
+  Swal.fire(JSON.stringify(formValues))
+}
+)
